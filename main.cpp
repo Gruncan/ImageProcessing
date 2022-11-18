750b43c71e7e92e7fe713dcffde57d4a9f61c592
Duncan Jones <dj223@outlook.com>
2022-11-18 20:47:54 +0000

    Updated main and gitignore

--- main.cpp
+++ main.cpp
@@ -1,31 +1,28 @@
 
 #include <fstream>
 #include <iostream>
+#include "Netpbm/PGM.h"
 
 
 using namespace std;
 
 int main() {
 
-    ofstream image;
-    image.open("image.ppm");
-    if (image.is_open()) {
-        //Header info
-        image << "P3" << endl;
-        image << "250 250" << endl;
-        image << "255" << endl;
-
-        for (int y = 0; y < 250; y++) {
-            for (int x = 0; x < 250; x++) {
-                image << x << " " << y << " " << (x * y) % 250 << endl;
-            }
-        }
-
-    }
+    Netpbm::PGM image = Netpbm::PGM(5, 5);
 
 
-    image.close();
+    int array[] = {0, 3, 3, 3, 3,
+                   0, 3, 0, 0, 0,
+                   0, 3, 3, 3, 0,
+                   0, 3, 0, 0, 0,
+                   0, 3, 0, 0, 0};
 
-    return 0;
+    image.setImageArray(array);
+
+//    std::cout << image;
+
+    Netpbm::exportImage(image, "test.pgm");
+
+
 }
 
